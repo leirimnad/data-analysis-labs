f3a42feaf9e693a765e5c4ef031c481b5f0aed8a
leirimnad <leirimnad.web@gmail.com>
2022-05-19 13:01:53 +0200

    regression analysis

--- regression_analysis/1_model.R
+++ regression_analysis/1_model.R
@@ -1,9 +1,25 @@
 options(scipen=5)
 songs <- read.csv(file = 'tiktok.csv')
 
-model <- lm(energy ~ popularity + loudness + tempo + popularity + danceability + acousticness + speechiness + valence + duration, data = songs)
+model <- lm(energy ~ 
+              popularity + loudness + tempo + popularity + danceability 
+              + acousticness + speechiness + valence + duration,
+            data = songs)
 cor.test(model$model$`energy`, model$fitted.values)
+
 summary(model)
+
+
+model <- lm(energy ~ 
+              popularity + loudness + tempo + popularity + danceability 
+            + acousticness + speechiness + valence,
+            data = songs)
+summary(model)
+
+confint(model)
+
+sigma(model)*100/mean(songs$energy)
+
 qqnorm(model$residuals)
 qqline(model$residuals, col="red")
 
